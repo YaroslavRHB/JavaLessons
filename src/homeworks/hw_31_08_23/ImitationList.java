54ea2a0d7d82a52ed43aea1b9f734b88ab80ab6f
Yaroslav RHB <zadesenets.yaroslav@gmail.com>
2023-09-02 01:00:13 +0100

    31.08 - first & second tasks

--- src/homeworks/hw_31_08_23/ImitationList.java
+++ src/homeworks/hw_31_08_23/ImitationList.java
@@ -56,6 +56,7 @@ public class ImitationList {
             return;
 
         }
+
         if (value == 0) {
 
             System.out.println("Zero not allowed");
@@ -98,6 +99,121 @@ public class ImitationList {
         // temp[i-1] = list[i];
 
         list = temp;
+    }
+
+    public void increaseDecreaseListByIndex(int index) {
+
+        if (list.length + index <= 0) {
+
+            System.out.println("Incorrect index");
+
+            return;
+        }
+
+        int[] temp = new int[list.length + index];
+
+        if (index > 0) {
+
+            for (int i = 0; i < list.length; i++) {
+
+                temp[i] = list[i];
+
+            }
+        }
+
+        if (index < 0) {
+
+            for (int i = 0; i < temp.length; i++) {
+
+                temp[i] = list[i];
+
+            }
+        }
+
+        list = temp;
+    }
+
+    public void changeListToFixedSize(int index) {
+
+        if (index <= 0) {
+
+            System.out.println("Incorrect index");
+
+            return;
+        }
+
+        int[] temp = new int[index];
+
+        if (temp.length > list.length) {
+
+            for (int i = 0; i < list.length; i++) {
+
+                temp[i] = list[i];
+
+            }
+        }
+
+        if (temp.length < list.length) {
+
+            for (int i = 0; i < temp.length; i++) {
+
+                temp[i] = list[i];
+
+            }
+        }
+
+        list = temp;
+    }
+
+    public void outputInOrder(int index) {
+
+        if (index != 0 && index != 1) {
+
+            System.out.println("Index should be 1 or 0");
+
+            return;
+        }
+
+        if (index == 0) {
+
+            for (int i = 0; i < list.length; i++) {
+
+                System.out.println(list[i]);
+
+            }
+        }
+
+        if (index == 1) {
+
+            for (int i = list.length - 1; i >= 0; i--) {
+
+                System.out.println(list[i]);
+            }
+        }
+    }
+
+    public void sortBubbleList() {
+
+        boolean isSorted = false;
+
+        while (!isSorted) {
+
+            isSorted = true;
+
+            for (int i = 1; i < list.length; i++) {
+
+                if (list[i] < list[i - 1]) {
+
+                    int temp = list[i];
+
+                    list[i] = list[i - 1];
+
+                    list[i - 1] = temp;
+
+                    isSorted = false;
+                }
+            }
+        }
     }
 
     public String toString() {

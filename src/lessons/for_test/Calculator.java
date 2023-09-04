9d0a9735e5de9c625b13128643c322c4ef04ce9b
Yaroslav RHB <zadesenets.yaroslav@gmail.com>
2023-09-04 18:39:04 +0100

    4.09 - Homework

--- src/lessons/for_test/Calculator.java
+++ src/lessons/for_test/Calculator.java
@@ -3,5 +3,11 @@ package lessons.for_test;
 public class Calculator {
     public int sum(int a, int b) {
         return a + b;
+    }public int difference(int a, int b) {
+        return a - b;
+    }public int product(int a, int b) {
+        return a * b;
+    }public double quotient(double a, double b) {
+        return a/b;
     }
 }
--- test/for_test/CalculatorTest.java
+++ test/for_test/CalculatorTest.java
@@ -9,33 +9,65 @@ public class CalculatorTest {
     public static void beforeAllTests() {
         System.out.println("Before all tests");
     }
+
     @AfterClass
     public static void afterAllTests() {
         System.out.println("After all tests");
     }
+
     @Before
     public void beforeEachTest() {
         System.out.println("Before each test");
     }
+
     @After
     public void afterEachTest() {
         System.out.println("After each test");
     }
 
     @Test
-    public void shouldReturnSumValues() {
+    public void shouldReturnSumOfValues() {
         Calculator calculator = new Calculator();
 
         int sum = calculator.sum(4, 5);
 
         Assert.assertEquals("Should return 9", 9, sum);
     }
+
     @Test
-    public void shouldReturnSumValues1() {
+    public void shouldReturnSumOfValues1() {
         Calculator calculator = new Calculator();
 
         int sum = calculator.sum(4, 5);
 
         Assert.assertEquals("Should return 9", 9, sum);
     }
+
+    @Test
+    public void shouldReturnDifferenceOfValues() {
+        Calculator calculator = new Calculator();
+
+        int difference = calculator.difference(4, 5);
+
+        Assert.assertEquals("Should return -1", -1, difference);
+    }
+
+    @Test
+    public void shouldReturnProductOfValues() {
+        Calculator calculator = new Calculator();
+
+        int product = calculator.product(4, 5);
+
+        Assert.assertEquals("Should return 20", 20, product);
+    }
+
+    @Test
+    public void shouldReturnQuotientOfValues() {
+        Calculator calculator = new Calculator();
+
+        double quotient = calculator.quotient(4, 5);
+
+        Assert.assertEquals(0.8, quotient, -1); // ???
+    }
+
 }
